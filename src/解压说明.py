16496a8bb7ff79fa28d21cbad23ec4c1919a0c38
APlus123 <906120662@qq.com>
2017-07-11 16:36:02 +0800

    OLLVM反混淆源码

--- src/解压说明.py
+++ src/解压说明.py
@@ -1,3 +1,10 @@
 #!usr/bin/python
 #coding=utf-8
-print "暂时只对内部公开所有反混淆源码，后续将放开所有限制，密码：我的名字"
+
+def unzip_instrucrion():
+
+	str= "暂时只对内部公开所有反混淆源码，后续将放开所有限制，密码：我的名字"
+	print str
+
+if __name__ == '__main__':
+	unzip_instrucrion()
